d19a6c7c852fcd27fc99524acab1bec141332376
sargsyanvahe <47384884+sargsyanvahe@users.noreply.github.com>
2020-07-10 19:33:44 +0400

    m

--- src/components/ModalContainer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ModalContainer(props) {
-    return (
-        <div className='modal-container'>
-            {props.children}
-        </div>
-    )
-}
