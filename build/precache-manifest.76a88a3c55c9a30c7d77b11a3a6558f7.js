da368a7e0f7d243ad7db91c2757cf53b6d75735a
sargsyanvahe <47384884+sargsyanvahe@users.noreply.github.com>
2020-07-10 18:27:31 +0400

    build added

---
+++ build/precache-manifest.76a88a3c55c9a30c7d77b11a3a6558f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1a2f3189c4ba25488d82f124b1a9c00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aebb85f5ef6775a4fdad",
+    "url": "/static/css/main.a7e54e8d.chunk.css"
+  },
+  {
+    "revision": "c20822341b9f5ba22bb0",
+    "url": "/static/js/2.36b849dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.36b849dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aebb85f5ef6775a4fdad",
+    "url": "/static/js/main.bd087e10.chunk.js"
+  },
+  {
+    "revision": "7e15eb7bc0d6469758af",
+    "url": "/static/js/runtime-main.d9bdf60d.js"
+  },
+  {
+    "revision": "8ce6f0644e5659f6b783c51d9128f623",
+    "url": "/static/media/Infinity-1s-200px.8ce6f064.svg"
+  },
+  {
+    "revision": "d3a7a2d6d61e23de9ae5289fdbe30548",
+    "url": "/static/media/Logo.d3a7a2d6.png"
+  }
+]);
